7d3c6c2aaac1ed3c75aaa8da6bc4ae8330c08f7e
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-11-24 14:30:09 -0500

    Added provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {useSelector} from "react-redux";
 
 function App() {
+  const counter = useSelector(state => state.counter);
   return (
     <div className="App">
+      <h1>Counter {counter}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { combineReducers, createStore } from "redux";
+import { createStore } from "redux";
 import rootReducer from "./reducers/index";
+import { Provider } from "react-redux";
 
-const store = createStore(combineReducers(rootReducer));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
